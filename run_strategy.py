66953bb14c14f70bc582034f5d711e5c82641441
LDH <343091787@qq.com>
2017-08-30 11:17:53 +0800

    conflict fixed

--- run_strategy.py
+++ run_strategy.py
@@ -8,13 +8,14 @@ Created on Tue Aug 22 08:33:34 2017
 # run_file.py
 
 config = {'base':
-    {'start_date':'20170501',
-     'end_date':'20170801',
+    {'start_date':'20140101',
+     'end_date':'20170501',
      'frequency':'1d',
      'capital':100000,
-     'universe':['600340']}}
+     'universe':['603881']}}
 
 from VectorTrader import run_file
+
 
 strategy_path = './test/test_moving_average.py'
 run_file(config,strategy_path,'p')
